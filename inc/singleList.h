77f61809235fdd663dc30f7be38165a747c8f70f
paweł.staniak <pawel.staniak@apsenergia.pl>
2019-01-07 15:22:54 +0100

    New files

---
+++ inc/singleList.h
@@ -0,0 +1,18 @@
+/*
+ * singleList.h
+ *
+ *  Created on: 7 sty 2019
+ *      Author: pawe³.staniak
+ */
+
+#ifndef INC_SINGLELIST_H_
+#define INC_SINGLELIST_H_
+
+typedef struct{
+	//Singly-Linked List
+	uint16_t *headSll;
+	uint16_t key;
+	uint16_t *nextPointer;
+}sll_t;
+
+#endif /* INC_SINGLELIST_H_ */
---
+++ src/singleList.c
@@ -0,0 +1,19 @@
+/*
+ * singleList.c
+ *
+ *  Created on: 7 sty 2019
+ *      Author: pawe³.staniak
+ */
+
+
+#include "main.h"
+
+sll_t list1;
+
+void sllInit (void){
+
+	list1.key=0U;
+	list1.headSll=NULL;
+	list1.nextPointer=NULL;
+
+}
